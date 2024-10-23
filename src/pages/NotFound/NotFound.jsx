afb69fc4245793499a749974976fea75e37772a5
TCO932 <chirkof.02@gmail.com>
2024-10-23 18:39:37 +0400

    NotFound styles

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return <div className='not_found'>
-    <p className="not_found__text">404 - Страница не найдена</p>
+    <h1 className="not_found__text">404 - Страница не найдена</h1>
     <Link to='/' className="not_found__home_link">Вернуться на главную</Link>
   </div>;
 };
